35c59fda4c9f4abfa8c29a958338dc6d2bf648b1
dark-gray <morozov07@bk.ru>
2015-11-19 00:11:33 +0400

    Delete week2.Lecture_2_6.scala

--- Lectures/.worksheet/src/week2.Lecture_2_6.scala
+++
@@ -1,6 +0,0 @@
-package week2
-
-object Lecture_2_6 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(91); 
-  println("Welcome to the Scala lecture 2.5 worksheet")}
-
-}
