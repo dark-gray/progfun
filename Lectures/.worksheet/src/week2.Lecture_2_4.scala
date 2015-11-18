fd8cf842113a92fad7150a9c684db33c2112148f
dark-gray <morozov07@bk.ru>
2015-11-19 00:11:16 +0400

    Delete week2.Lecture_2_4.scala

--- Lectures/.worksheet/src/week2.Lecture_2_4.scala
+++
@@ -1,6 +0,0 @@
-package week2
-
-object Lecture_2_4 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(91); 
-  println("Welcome to the Scala lecture 2.4 worksheet");$skip(26); val res$0 = 
-	new week2.Rational(1, 2);System.out.println("""res0: week2.Rational = """ + $show(res$0))}
-}
